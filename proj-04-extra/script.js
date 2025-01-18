78376007a59b5fd05733dd9bf8924557bacd913d
Layza Vitoria <layzanogueira37@gmail.com>
2025-01-18 14:16:28 -0300

    pronto
    a base foi feita, so falta por a lista

--- proj-04-extra/script.js
+++ proj-04-extra/script.js
@@ -1,12 +1,32 @@
 let num = document.getElementById("num");
+let nome = document.getElementById("name");
+
+let displayNumeroNome = document.querySelector(".numero");
+
+let lista = {
+  11: "layza",
+  20: "vitoria",
+  3: "fraramis",
+};
 
 function sortear() {
-  let lista = [20, 99, 10, 2, 3, 4, 5];
+  let chaves = Object.keys(lista);
 
-  let indiceAleatorio = Math.floor(Math.random() * lista.length);
-  let numAleatorio = lista[indiceAleatorio];
+  let indiceAleatorio = Math.floor(Math.random() * chaves.length);
+  let numAleatorio = chaves[indiceAleatorio];
 
   console.log(numAleatorio);
 
+  let nomeDoNumero = lista[numAleatorio];
+
+  console.log(nomeDoNumero);
+
   num.innerText = numAleatorio;
+  nome.innerText = nomeDoNumero;
+
+  displayNumeroNome.style.display = "flex";
+
+  setTimeout(() => {
+    displayNumeroNome.classList.add("visible");
+  }, 10);
 }
